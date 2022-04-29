6b9c5b220d0b59327329395adf7b3a3606cad2ac
Vladimir Artemchenko <voartemchenko@gmail.com>
2022-04-29 14:41:34 +0600

    refactor: selection login component, add types for setStateAction

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import Board from "./components/Board/Board";
+import Modal from "./components/Modal/Modal";
+import Login from "./components/Login/Login";
 import styled from "styled-components";
 import {v4 as uuidv4} from "uuid";
 
@@ -12,6 +14,8 @@ const initialColumnsName = [
 
 const App: React.FC = () => {
 
+    const [userName, onSetUserName] = useState<string>('');
+    const [isLoginModalActive, onSetModalActive] = useState<boolean>(true);
     const [value, onSetValue] = useState<string>('');
     const [columns, onSetColumns] = useState(initialColumnsName);
 
@@ -20,7 +24,6 @@ const App: React.FC = () => {
     };
 
     const createNewColumn = () => {
-
         if (value) {
             onSetColumns([...columns, {id: uuidv4(), title: value}])
             onSetValue('')
@@ -32,17 +35,21 @@ const App: React.FC = () => {
 
         <Root>
 
-            <NewBoard type="text" onChange={handleChange} value={value}/>
+            <Modal isActive={isLoginModalActive}>
+                <Login userName={userName} onSetUserName={onSetUserName} onSetModalActive={onSetModalActive}/>
+            </Modal>
 
-            <NewBoardButton onClick={createNewColumn}>Add Board</NewBoardButton>
+            <NewColumn type="text" onChange={handleChange} value={value}/>
 
-            <Boards>
+            <NewColumnButton onClick={createNewColumn}>Add Board</NewColumnButton>
+
+            <Columns>
                 {columns.map((el) => {
                     return (
                         <Board key={el.id} title={el.title}/>
                     )
                 })}
-            </Boards>
+            </Columns>
 
         </Root>
     )
@@ -50,13 +57,13 @@ const App: React.FC = () => {
 
 export default App
 
-const Boards = styled.div`
+const Columns = styled.div`
   display: flex;
   width: 90%;
   box-sizing: border-box;
   margin: 20px;
 `;
-const NewBoard = styled.input`
+const NewColumn = styled.input`
   width: 400px;
   height: 30px;
   border: none;
@@ -67,7 +74,7 @@ const Root = styled.div`
   flex-direction: column;
   width: 100%;
 `;
-const NewBoardButton = styled.button`
+const NewColumnButton = styled.button`
   font-size: 18px;
   border: none;
   border-radius: 5px;
