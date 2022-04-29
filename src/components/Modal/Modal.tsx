ea8f2b6facd69063318135a5f3d377e4862c27a7
Vladimir Artemchenko <voartemchenko@gmail.com>
2022-04-29 14:41:34 +0600

    refactor: selection login component, add types for setStateAction

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ModalProps {
+    isActive: boolean;
+    children:JSX.Element;
+}
+
+ const Modal: React.FC<ModalProps> = ({isActive, children}) => {
+
+    return (
+
+        <Root isActive={isActive}>{children}</Root>
+
+    )
+}
+
+export default Modal
+
+const Root = styled.div<ModalProps>`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.4);
+  visibility: ${props => props.isActive ? "visible" : "hidden"};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
